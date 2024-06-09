c9af2f8ab12e203fe00af76b0a14fd4113f9b9a0
shubhammani41 <shubhammani41@gmail.com>
2024-06-09 11:18:39 +0530

    fixed skeleton and dev images

---
+++ src/components/profileSkeleton/profileSkeleton.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import "./profileSkeleton.scss";
+import { Skeleton } from "@mui/material";
+
+const ProfileSkeleton: React.FC = ()=>{
+    return (
+        <div className="skeleton-container">
+            <div className="df js ac gp30px mrg">
+                <Skeleton variant="circular" width={100} height={100} animation="wave" />
+                <div className="skeleton-avatar-title">
+                    <Skeleton variant="text" sx={{ fontSize: '1rem' }} />
+                    <Skeleton variant="text" sx={{ fontSize: '1rem' }} />
+                </div>
+            </div>
+            <Skeleton variant="rounded" width={400} height={160} animation="wave" />
+        </div>
+    )
+}
+
+export {ProfileSkeleton}
--- src/modules/homeModule/homeComponent/homeComponent.tsx
+++ src/modules/homeModule/homeComponent/homeComponent.tsx
@@ -1,15 +1,17 @@
-import { Button, Card, CardActions, CardContent, CardMedia, Skeleton, TablePagination, TextField, Typography } from "@mui/material";
+import { Avatar, Button, Card, CardActions, CardContent, CardHeader, CardMedia, Skeleton, TablePagination, TextField, Typography } from "@mui/material";
 import React, { RefObject, useEffect, useState } from "react";
 import clubbedToDeath from '../../../assets/audio/clubbedToDeath.mp3';
 import './homeComponent.scss';
 import { UserData } from "../../../models/userData";
 import axiosInstance from "../../../config/axiosConfig";
+import { ProfileSkeleton } from "../../../components/profileSkeleton/profileSkeleton";
 
 const HomeComponent: React.FC = () => {
     const audioRef: RefObject<HTMLAudioElement> = React.createRef();
     const [devDataList, setDevData] = useState<UserData[]>([]);
     const [rowsPerPage, setRowsPerPage] = useState<number>(5);
     const [pageNumber, setPageNumber] = useState<number>(0);
+    const [totalElements, setTotalElements] = useState<number>(0);
     const [devListLoading, setDevListLoading] = useState<boolean>(false);
 
     const handleChangePage = (event: unknown, newPage: number) => {
@@ -29,9 +31,10 @@ const HomeComponent: React.FC = () => {
     const fetchUserData = async (rowsPerPage: number, pageNumber: number) => {
         setDevListLoading(true);
         try {
-            const response = await axiosInstance.get(`public/getRandomUserList?pageSize=${rowsPerPage}&pageNumber=${pageNumber}`);
-            console.log(response.data.data.data)
-            setDevData(response.data.data.data.map((obj: any) => {
+            let response:any = await axiosInstance.get(`public/getRandomUserList?pageSize=${rowsPerPage}&pageNumber=${pageNumber}`);
+            response = response.data.data;
+            setTotalElements(response.totalElements);
+            setDevData(response.data.map((obj: any) => {
                 return { ...obj, skills: JSON.parse(obj.skills).map((sk: any) => sk.skill_name).join(",") }
             }));
         } catch (error) {
@@ -46,7 +49,7 @@ const HomeComponent: React.FC = () => {
     }, []);
 
     return (
-        <div>
+        <div className="mainContainer">
             {/* <div className="app-background-image-container">
                 <img className="app-background-image" src={matrixrain} />
             </div> */}
@@ -66,40 +69,43 @@ const HomeComponent: React.FC = () => {
                 />
             </div>
             <div className="matrix-card-list">
-                {devListLoading?
-                    <div className="df jc ac fw gp50px w65vw">
-                        {Array(4).fill(1).map(() => {
-                            return (
-                                <div className="skeleton-container">
-                                    <Skeleton variant="circular" width={100} height={100} animation="wave"/>
-                                    <Skeleton variant="rounded" width={380} height={200} animation="wave"/>
-                                </div>
-                            )
+                {devListLoading ?
+                    <div className="df jc ac fw gp50px w90vw">
+                        {Array(3).fill(1).map((val,index) => {
+                            return (<ProfileSkeleton key={"profileSkeleton_"+index}></ProfileSkeleton>)
                         })}
                     </div> :
                     <div className="df jc ac fw gp50px w90vw">
                         {devDataList.map((devData) => {
                             return (
-                                <div className="matrix-card-container">
+                                <div className="matrix-card-container" key={"dev_"+devData.userId}>
                                     <Card>
-                                        <CardMedia
+                                        {/* <CardMedia
                                             component="img"
                                             alt={"Image not found"}
                                             height="140"
                                             src={devData.profilePictureUrl ? devData.profilePictureUrl : "defaultImageUrl"}
-                                        />
+                                        /> */}
+                                        <div className="df js ac gp30px m15">
+                                            <Avatar className="avatar100" alt={devData.firstName || ""} src={devData.profilePictureUrl || ""} />
+                                            <div>
+                                                <Typography className="w300p ellipsis" gutterBottom variant="h5" component="div">
+                                                    {devData.firstName ? devData.firstName : ""} {devData.lastName ? devData.lastName : ""}
+                                                </Typography>
+                                                <Typography className="w300p ellipsis" variant="body2" color="text.secondary">
+                                                    {devData.positionName}
+                                                </Typography>
+                                            </div>
+                                        </div>
                                         <CardContent>
-                                            <Typography className="w300p ellipsis" gutterBottom variant="h5" component="div">
-                                                {devData.firstName ? devData.firstName : ""} {devData.lastName ? devData.lastName : ""}
-                                            </Typography>
-                                            <Typography className="w300p ellipsis" variant="body2" color="text.secondary">
-                                                {devData.positionName}
-                                            </Typography>
                                             <Typography className="w300p ellipsis" variant="body2" color="text.secondary">
                                                 Experience: {devData.experience} years
                                             </Typography>
                                             <Typography className="w300p ellipsis" variant="body2" color="text.secondary">
                                                 Technology: {devData.skills}
+                                            </Typography>
+                                            <Typography className="w300p ellipsis" variant="body2" color="text.secondary">
+                                                Location: {devData.cityName ? devData.cityName + "," : ""} {devData.stateName ? devData.stateName + "," : ""} {devData.cityName ? devData.countryName + "," : ""}
                                             </Typography>
                                         </CardContent>
                                         <CardActions>
@@ -111,18 +117,24 @@ const HomeComponent: React.FC = () => {
                         })}
                     </div>}
             </div>
-            {!devListLoading?
-            <div className="df jc ac fw gp50px paginator-container">
-                <TablePagination
-                rowsPerPageOptions={[5, 10, 25]}
-                count={devDataList.length}
-                rowsPerPage={rowsPerPage}
-                page={pageNumber}
-                onPageChange={handleChangePage}
-                onRowsPerPageChange={handleChangeRowsPerPage}
-            />
-            </div>
-            :null}
+            {!devListLoading ?
+                <div className="df jc ac fw gp50px paginator-container">
+                    <table>
+                        <tbody>
+                        <tr>
+                        <TablePagination
+                        rowsPerPageOptions={[5, 10, 25]}
+                        count={totalElements}
+                        rowsPerPage={rowsPerPage}
+                        page={pageNumber}
+                        onPageChange={handleChangePage}
+                        onRowsPerPageChange={handleChangeRowsPerPage}
+                    />
+                        </tr>
+                        </tbody>
+                    </table>
+                </div>
+                : null}
             <audio ref={audioRef} loop controls={false} autoPlay={true}>
                 <source src={clubbedToDeath} type="audio/mp3" />
             </audio>
